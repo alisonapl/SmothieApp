7f4e08929c58f5c45668dd3dbac888fe5640351c
alisonapl <125928888+alisonapl@users.noreply.github.com>
2023-05-12 16:03:12 -0600

    Add spaces

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ function init() {
 	const TOPPINGS = 3;
 	var tipoDeListaActual = FRUTAS;
 
+
+
+
+
 	var mainSubTitle = document.getElementById('mainSubTitle');
 	var frutasContainer = document.getElementById('frutas_container');
 	var frustasBtn = document.getElementById('frustasBtn');
