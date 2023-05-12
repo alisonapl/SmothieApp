a8f81f980d71fa93a607e96e436999bf4a9d7d6d
alisonapl <125928888+alisonapl@users.noreply.github.com>
2023-05-12 15:58:31 -0600

    Add initial files.

---
+++ ingredienteComponent.js
@@ -0,0 +1,70 @@
+export class IngredienteComponent {
+    constructor(frutasContainer, ingrediente, callback, contador) {
+        this.ingrediente = ingrediente;
+        this.callback = callback;
+        this.contador = contador;
+        var container = document.createElement("div");
+        frutasContainer.appendChild(container);
+        container.className = "fruta_container";
+
+        var foto = document.createElement("div");
+        container.appendChild(foto);
+        foto.className = "fruta_foto_container";
+
+        var img = document.createElement("img");
+        foto.appendChild(img);
+        img.src = ingrediente.foto;
+        img.className = "fruta_foto";
+
+        var textos = document.createElement("div");
+        container.appendChild(textos);
+        textos.className = "fruta_textos_container";
+
+        var nombre = document.createElement("p");
+        nombre.innerHTML = ingrediente.nombre;
+        textos.appendChild(nombre);
+        nombre.className = "fruta_name";
+
+        var precio = document.createElement("p");
+        precio.innerHTML = "Precio: " + ingrediente.precio;
+        textos.appendChild(precio);
+        precio.className = "fruta_precio";
+
+        var disponibleP = document.createElement("p");
+        disponibleP.className = "fruta_disponible";
+
+        if (ingrediente.disponible) {
+            disponibleP.innerHTML = "Disponible: Si";
+        } else {
+            disponibleP.innerHTML = "Disponible: No";
+            disponibleP.classList.add("fruta_disponible_no");
+        }
+
+        textos.appendChild(disponibleP);
+
+        this.cantidad = document.createElement('p');
+        textos.appendChild(this.cantidad);
+        this.cantidad.innerHTML = 'Cantidad: ' + this.contador;
+        this.cantidad.className = 'fruta_cantidad';
+
+        var botones = document.createElement("div");
+        container.appendChild(botones);
+        botones.className = "fruta_button_container";
+
+        var agregarBtn = document.createElement('button');
+        agregarBtn.innerHTML = 'Agregar'
+        agregarBtn.className = 'frutas_agregarBtn';
+        botones.appendChild(agregarBtn);
+        agregarBtn.onclick = this.onAgregarBtn.bind(this);
+        agregarBtn.disabled = !this.ingrediente.disponible;
+        if (!ingrediente.disponible) {
+            agregarBtn.classList.add('frutas_agregarBtn_disabled');
+        }
+    }
+
+    onAgregarBtn() {
+        this.callback(this.ingrediente);
+        this.contador += 1;
+        this.cantidad.innerHTML = 'Cantidad: ' + this.contador;
+    }
+}
---
+++ main.js
@@ -0,0 +1,110 @@
+import { IngredienteComponent } from '/ingredienteComponent.js';
+
+window.addEventListener('load', init);
+function init() {
+
+	const FRUTAS = 1;
+	const VERDURAS = 2;
+	const TOPPINGS = 3;
+	var tipoDeListaActual = FRUTAS;
+
+	var mainSubTitle = document.getElementById('mainSubTitle');
+	var frutasContainer = document.getElementById('frutas_container');
+	var frustasBtn = document.getElementById('frustasBtn');
+	var verdurasBtn = document.getElementById('verdurasBtn');
+	var toppingsBtn = document.getElementById('toppingsBtn');
+	var resetBtn = document.getElementById('resetBtn');
+
+	var ingredientesSeleccionados = [];
+	var data = null;
+
+	loadIngredientes();
+
+	frustasBtn.onclick = onFrutasBtn;
+	verdurasBtn.onclick = onVerdurasBtn;
+	toppingsBtn.onclick = onToppingsBtn;
+	resetBtn.onclick = onResetBtn;
+
+	function mostrarIngredientesPorTipo() {
+
+		var ingredientes = [];
+
+		switch (tipoDeListaActual) {
+			case FRUTAS:
+				ingredientes = data.frutas;
+				break;
+			case VERDURAS:
+				ingredientes = data.verduras;
+				break;
+			case TOPPINGS:
+				ingredientes = data.toppings;
+				break;
+			default:
+				break;
+		}
+
+		frutasContainer.innerHTML = "";
+
+		for (let index = 0; index < ingredientes.length; index++) {
+			const ingrediente = ingredientes[index];
+			var cantidad = contarIngredientesPorNombre(ingrediente.nombre);
+			var ingredienteComponent = new IngredienteComponent(frutasContainer, ingrediente, agregarIngrediente, cantidad);
+		}
+	}
+
+	function loadIngredientes() {
+		var request = new XMLHttpRequest();
+		request.open('GET', 'https://smothieapp-default-rtdb.firebaseio.com/data.json');
+		request.send();
+		request.onload = function (event) {
+			var jsonData = request.responseText;
+			data = JSON.parse(jsonData);
+			mostrarIngredientesPorTipo();
+		}
+	}
+
+	function agregarIngrediente(ingrediente) {
+		ingredientesSeleccionados.push(ingrediente);
+		console.log(ingredientesSeleccionados);
+		var total = 0;
+		for (let index = 0; index < ingredientesSeleccionados.length; index++) {
+			const fruta = ingredientesSeleccionados[index];
+			total += fruta.precio;
+		}
+
+		mainSubTitle.innerHTML = "Smoothie Price: " + total;
+	}
+
+	function onFrutasBtn() {
+		tipoDeListaActual = FRUTAS;
+		mostrarIngredientesPorTipo();
+	}
+
+	function onVerdurasBtn() {
+		tipoDeListaActual = VERDURAS;
+		mostrarIngredientesPorTipo();
+	}
+
+	function onToppingsBtn() {
+		tipoDeListaActual = TOPPINGS;
+		mostrarIngredientesPorTipo();
+	}
+
+	function onResetBtn() {
+		ingredientesSeleccionados = [];
+		mainSubTitle.innerHTML = "Smoothie Price: 0";
+		mostrarIngredientesPorTipo();
+	}
+
+	function contarIngredientesPorNombre(nombre) {
+		var total = 0;
+		for (let index = 0; index < ingredientesSeleccionados.length; index++) {
+			const ingrediente = ingredientesSeleccionados[index];
+			if (ingrediente.nombre === nombre) {
+				total += 1;
+			}
+		}
+		return total;
+	}
+}
+
